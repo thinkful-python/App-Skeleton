bc6ebe2f9f984194735dc9926f2ecdd5f8f72979
dom <dom@thedom.io>
2015-05-28 06:25:07 +0000

    adding snippets.py with put and get definitions

---
+++ snippets.py
@@ -0,0 +1,13 @@
+import logging
+
+# Set the log output file, and the log level
+logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
+
+def put(name, snippet): 
+	"""Store a snippet with an associated name. Returns the name and the snippet"""
+	logging.error("FIXME: Unimplemented - put ({!r}, {!r})".format(name, snippet)
+	return name, snippet
+								
+def get(name):
+
+								
