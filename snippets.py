aa70029cffb9f340ce6cda3ea99da6d4de980137
dom <dom@thedom.io>
2015-06-03 03:02:08 +0000

    adding more subparse arguments for put

--- snippets.py
+++ snippets.py
@@ -23,6 +23,14 @@ def main():
 	"""Main function"""
 	logging.info("Constructing parser")
 	parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
+	
+	subparsers = parser.add_subparsers(dest="command", help="Available commands")
+	
+	#subparser for the put command
+	logging.debug("Constructing put subparser")
+	put_parser = subparsers.add_parser("put", help= "Store a snippet")
+	put_parser.add_argument("name", help="The name of the snippet")
+	put_parser.add_argument("snippet", help="The snippet text")
 	arguments = parser.parse_args(sys.argv[1:])
 	
 if __name__ == "__main__":
